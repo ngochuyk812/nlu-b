171e21110fe296dba773d3cd44f3e4079f8ba3f0
ngochuyk812 <ngohuyk80169@gmail.com>
2023-02-03 08:55:37 -0800

    Update

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function compareDates(d1, d2, now){
   return d1 <= now && now <=  d2;
   }
 
-const login = async ()=>{
+const login = async (user, pass)=>{
   const response = await fetch('https://dkmh.hcmuaf.edu.vn/api/auth/login', {
     method: 'POST',
     headers: {
@@ -41,23 +41,22 @@ const login = async ()=>{
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"'
     },
-    body: 'username=20130282&password=saimatkhau1&grant_type=password'
+    body: `username=${user}&password=${pass}&grant_type=password`
 });
 return response.text();
 }
 
-app.get("/login", async(req,res)=>{
- const body = await login()
-
-res.send(JSON.parse(body))
+app.post("/login/", async(req,res)=>{
+ const body = await login(req.body.username, req.body.pass)
+  console.log(req.body.username, req.body.pass);
+res.send(JSON.stringify(body))
 })
-app.get("/tkb", async (req,res)=>{
-  const tokenUser = await login()
-  let data = []
+app.post("/tkb", async (req,res)=>{
+  console.log(req.body.token);
 var headers = {
   'Accept': 'application/json, text/plain, */*',
   'Accept-Language': 'en-US,en;q=0.9,vi;q=0.8',
-  'Authorization': 'Bearer '+ JSON.parse(tokenUser).access_token,
+  'Authorization': 'Bearer '+ req.body.token,
   'Connection': 'keep-alive',
   'Content-Type': 'application/json',
   'Cookie': 'ASP.NET_SessionId=qqa2epgjh2xrlvl2e5tnrbfd',
@@ -84,22 +83,22 @@ var options = {
 function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
       let listAll = JSON.parse(body).data.ds_tuan_tkb
-      var date = moment();
-      var now = date.format('DD/MM/YYYY');
-      for(let i =0 ; i <listAll.length ; i++){
-        let element = listAll[i]
-        if(compareDates(element.ngay_bat_dau, element.ngay_ket_thuc,now)){
-          data.push(element)
-          data.push(listAll[i+1])
-          break
-        }
-      }
+      // var date = moment();
+      // var now = date.format('DD/MM/YYYY');
+      // for(let i =0 ; i <listAll.length ; i++){
+      //   let element = listAll[i]
+      //   if(compareDates(element.ngay_bat_dau, element.ngay_ket_thuc,now)){
+      //     data.push(element)
+      //     data.push(listAll[i+1])
+      //     break
+      //   }
+      // }
       
-      res.send(JSON.stringify(data))
+      res.send(JSON.stringify(listAll))
   }
 }
 
 request(options, callback);
 })
 // Khởi tạo server tại port 4000
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 2011);
